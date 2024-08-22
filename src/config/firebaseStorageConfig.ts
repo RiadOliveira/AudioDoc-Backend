94616669ca3b8936fad11c74e99f3140b861de14
Lucas Paulino <lucaspaulinoh@hotmail.com>
2024-08-22 17:16:37 -0300

    FEAT: adding firebase configuration

---
+++ src/config/firebaseStorageConfig.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyASlsRwEFWjgDsLKuvRzEOSCxodjAi9L-c',
+  authDomain: 'audiodoc-storage.firebaseapp.com',
+  projectId: 'audiodoc-storage',
+  storageBucket: 'audiodoc-storage.appspot.com',
+  messagingSenderId: '55510653823',
+  appId: '1:55510653823:web:7561516ccc0e3a0d2bd84a',
+};
+
+const app = initializeApp(firebaseConfig);
