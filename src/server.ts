2119cc7cce8163156c02b933f790ca6df64177c2
Ríad Oliveira <riad.oliveira@hotmail.com>
2024-09-23 11:23:17 -0300

    CORS fixed

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import routes from 'routes';
 import fastify from 'fastify';
+import cors from '@fastify/cors';
 import fileHandler from '@fastify/multipart';
 import fileHandlerOptions from 'config/fileHandlerOptions';
+import loggerConfig from 'config/fastifyLoggerConfig';
 
-const server = fastify();
+const server = fastify(loggerConfig);
+
 server.register(fileHandler, fileHandlerOptions);
 server.register(routes);
+server.register(cors, { origin: '*', methods: ['POST'] });
 
 server.listen({ port: 3000 }, (error, _address) => {
   if (!error) return;
